2afda9a459005e430030fe9e0f410451f3735a61
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-12-27 18:35:54 +0530

    Update settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i^+p(k#)*9+ftcxvic&7@=x8=h%8%708*3-96k0#e)na8eyob6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
